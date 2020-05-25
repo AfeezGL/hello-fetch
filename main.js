6a817aecba888cd9ba3197787bc1050a8fec6a2c
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2020-05-25 15:12:07 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+var searchQuery = document.getElementById("searchQuery")
+var output = document.getElementById("output")
+
+document.getElementById("search").addEventListener("click", getUser)
+
+function getUser(e){
+  e.preventDefault();
+  fetch("https://api.github.com/users/"+searchQuery.value)
+  .then(res => res.json())
+  .then(user => {
+    var card = `
+    <div class="card">
+      <img src="${user.avatar_url}" class="img card-img-top">
+      <div class="card-body">
+        <h6 class="card-heading">${user.name}</h6>
+        <p class="card-text">This user was gotten from github API using JavaScript FetchAPI</p>
+        <a href="${user.html_url}" class="btn btn-primary" target="_blank">View Profile</a>
+      </div>
+    </div>
+    `
+    output.innerHTML = card;
+  });
+};
