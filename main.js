03c18747bd4d36a230739063b2019fc81148f2aa
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2020-05-25 15:57:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-var searchQuery = document.getElementById("searchQuery")
-var output = document.getElementById("output")
+var searchQuery = document.getElementById('searchQuery');
+var output = document.querySelector("#output");
 
-document.getElementById("search").addEventListener("click", getUser)
+document.getElementById('search').addEventListener('click', getUser);
 
 function getUser(e){
-  e.preventDefault();
-  fetch("https://api.github.com/users/"+searchQuery.value)
-  .then(res => res.json())
-  .then(user => {
-    var card = `
-    <div class="card" style="width: 21rem;">
-      <img src="${user.avatar_url}" class="img card-img-top" style="max-height: 21rem;">
-      <div class="card-body">
-        <h5 class="card-heading">${user.name}</h5>
-        <p class="card-text">This user was gotten from github API using JavaScript FetchAPI</p>
-        <a href="${user.html_url}" class="btn btn-primary" target="_blank">View Profile</a>
-      </div>
-    </div>
-    `
-    output.innerHTML = card;
-  });
+    e.preventDefault();
+    fetch('https://api.github.com/users/'+searchQuery.value)
+    .then(res => res.json())
+    .then((user) => {
+    	if (user.name) {
+    		var card = `
+    			<div class="card" style="width: 21rem;">
+    				<img src="${user.avatar_url}" class="card-img-top img-fluid img" style="max-height: 21rem;">
+    				<div class="card-body">
+    					<h5 class="card-title">${user.name}</h5>
+    					<p class="card-text">This user was gotten through github API using javascript fetchAPI</p>
+    					<a href="${user.html_url}" class="btn btn-primary">View Profile</a>
+    				</div>
+    			</div>
+    	`
+    	output.innerHTML = card;
+    	} else{
+    		var card = `
+    		<div class="card" style="width: 21rem;">
+    			<div class="card-body">
+    				<p class="card-text">User ${searchQuery.value} does not exist.</p>
+    			</div>
+    		</div>
+    		`
+    		output.innerHTML = card;
+    	}
+    });
 };
